50516e000288fd226c5ff77b77d82af9319a9fbb
Petrichor2390 <cfawcett2002@gmail.com>
2024-09-03 18:50:59 +1000

    waffle_pi working in environment

--- launch/factory_world.launch.py
+++ launch/factory_world.launch.py
@@ -10,16 +10,16 @@ def generate_launch_description():
     launch_dir = os.path.join(get_package_share_directory('smart_factory'), 'launch')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
-    use_sim_time = LaunchConfiguration('use_sime_time', default='true')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     x_pose = LaunchConfiguration('x_pose', default='0')
     y_pose = LaunchConfiguration('y_pose', default='0')
 
-    world = os.path.join(get_package_share_directory('smart_factory'), 'worlds', 'factory.world')
+    world = os.path.join(get_package_share_directory('smart_factory'), 'worlds', 'turtlebot3_office3.world')
     
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
-        ), launch_arguments={'worlds': world}.items()
+        ), launch_arguments={'world': world}.items()
     )
     
     gzclient_cmd = IncludeLaunchDescription(
@@ -28,8 +28,28 @@ def generate_launch_description():
         )
     )
 
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_dir, 'robot_state_publisher.launch.py')
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    spawn_turtlebot_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_dir, 'spawn_turtlebot3.launch.py')
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose
+        }.items()
+    )
+
     ld = LaunchDescription()
+    
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
+    ld.add_action(robot_state_publisher_cmd)
+    ld.add_action(spawn_turtlebot_cmd)
 
     return ld
