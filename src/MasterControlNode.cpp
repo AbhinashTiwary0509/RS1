ac6e4046b1df1185e57ea6c46c0db8e754014215
Petrichor2390 <cfawcett2002@gmail.com>
2024-10-26 18:22:37 +1100

    Master Control Node working with multiple objects and storage positions

--- src/MasterControlNode.cpp
+++ src/MasterControlNode.cpp
@@ -60,10 +60,14 @@ public:
     ObjectStorage(){
         //where the objects are at the start of the run
         //example inout area object
-        createObject(0, 1.1, 2);
+        createObject(0, 1.6, -0.8);
+        createObject(1, 1, -0.8);
+        createObject(2, 0.4, -0.8);
 
         //create all of the valid storage positions
-        addStoragePosition(1.6, -0.8, 0);
+        addStoragePosition(1.5, 1.4, 0);
+        addStoragePosition(1.5, 2.1, 0);
+        addStoragePosition(1.5, 2.8, 0);
     }
 
     void createObject(int ID, double xPos, double yPos){
@@ -95,7 +99,7 @@ public:
         }
     }
 
-    bool generateGoalForObjectPickup(goalStruct &goal){ //true if there is a goal false otherwise, will pass the goal by reference
+    bool generateGoalForObjectPickup(goalStruct &goal, int &Obj_ID){ //true if there is a goal false otherwise, will pass the goal by reference
         int indexTarget = -1;
         for(size_t i = 0; i < objects_.size(); i++){
             if(objects_.at(i).stored == false && objects_.at(i).targeted == false){
@@ -106,10 +110,11 @@ public:
             std::cout << "No object to target" << std::endl;
             return false;
         }else{
+            Obj_ID = objects_.at(indexTarget).ID;
             goal.xPos = objects_.at(indexTarget).xPos;
             goal.yPos = objects_.at(indexTarget).yPos;
             goal.type = INPUT_GOAL;
-            goal.yaw = 0; //default yaw for target is 0 change later to fit input area
+            goal.yaw = 90*(M_PI/180); //default yaw for target is 0 change later to fit input area
             objects_.at(indexTarget).targeted = true;
             return true;
         }
@@ -117,13 +122,16 @@ public:
         return false; //default
     }
 
-    goalStruct getAvailableStoragePosition(){
+    bool getAvailableStoragePosition(goalStruct &goal){ //true for storage available false for no storage available
         for(size_t i = 0; i< storagePositions_.size(); i++){
-            if(storagePositions_.at(i).empty = true){
+            if(storagePositions_.at(i).empty == true){
                 storagePositions_.at(i).empty = false; //if this function is called to get this position it is automatically considered full
-                return storagePositions_.at(i).position;
+                goal = storagePositions_.at(i).position;
+                return true;
             }
         }
+        std::cout << "ERR_ No Storage Positions available" << std::endl;
+        return false;
     }
 
 private:
@@ -161,7 +169,8 @@ public:
 
         robotID_ = 0;
 
-        heldObjectID_ = NULL;
+        heldObjectID_ = -1;
+        objectHeld_ = false;
     }
 
     int getStatus(){
@@ -192,14 +201,24 @@ public:
     }
 
     void flagGoalAsAchieved(){
-        status_ = WAITING;
+        if(heldObjectID_ != -1){
+            status_ = WAITING_WITH_OBJECT;
+            std::cout << "[ROBOT] status: WAITING_WITH_OBJECT" << std::endl;
+        }else{
+            status_ = WAITING;
+            std::cout << "[ROBOT] status: WAITING" << std::endl;
+        }
     }
 
-    void pickupDropoffObjectToggle(bool pickupDropoff, int ID){ //true for object being picked up false otherwise
+    void pickupDropoffObjectToggle(bool pickupDropoff, int ID = -1){ //true for object being picked up false otherwise
         if(pickupDropoff){
+            objectHeld_ = true;
+            std::cout << "[ROBOT] picked up object" << std::endl;
             heldObjectID_ = ID;
         }else{
-            heldObjectID_= NULL;
+            objectHeld_ = false;
+            std::cout << "[ROBOT] dropping off object" << std::endl;
+            heldObjectID_= -1;
         }
     }
 
@@ -208,6 +227,7 @@ private:
     goalStruct currentGoal_;
     int robotID_;
     int heldObjectID_;
+    bool objectHeld_;
 };
  
 class MasterControlNode : public rclcpp::Node {
@@ -299,18 +319,23 @@ private:
 
         goalStruct newManagerGoal;
         bool availableObject = false;
+        bool availableStorage = false;
+        int objID = -1;
         
         switch (robots_.at(targetRobotID_).getStatus())
         {
         case WAITING:
             //send to input area box to pickup
-            availableObject = storage_.generateGoalForObjectPickup(newManagerGoal);
+            availableObject = storage_.generateGoalForObjectPickup(newManagerGoal, objID);
             if(!availableObject){
                 std::cout << "Robot waiting, no objects to move" << std::endl;
                 return; //nothing to do so return
             }else{
                 //send goal
+                std::cout << "Sending Goal - PICKUP object" << std::endl;
+                robots_.at(targetRobotID_).setGoal(newManagerGoal);
                 sendGoal(newManagerGoal);
+                robots_.at(targetRobotID_).pickupDropoffObjectToggle(true, objID);
                 robots_.at(targetRobotID_).setGoal(newManagerGoal);
             }
             break;
@@ -324,6 +349,18 @@ private:
             break;
 
         case WAITING_WITH_OBJECT:
+            availableStorage = storage_.getAvailableStoragePosition(newManagerGoal);
+            if(!availableStorage){
+                std::cout << "Robot waiting, no storage positions" << std::endl;
+                return;
+            }else{
+                std::cout << "Sending Goal - STORE object" << std::endl;
+                robots_.at(targetRobotID_).setGoal(newManagerGoal);
+                sendGoal(newManagerGoal);
+                robots_.at(targetRobotID_).pickupDropoffObjectToggle(false);
+                robots_.at(targetRobotID_).setGoal(newManagerGoal);
+            }
+            
             /* code */
             break;
         
